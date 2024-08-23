5a9f0aa74aaac07b207d6ad65ccd9baec4613669
Bruno Henrique <brunohenriquef.pinto@gmail.com>
2024-08-23 00:54:01 -0300

    update
    update do header, add propriedade img nos objetos cosméticos e remédios.

---
+++ database/cosmeticos.js
@@ -0,0 +1,25 @@
+let listaCosmeticos = [
+    { nome: "Shampoo Pantene", preco: 15.99, img: "shampoo-pantene.png" },
+    { nome: "Condicionador TRESemmé", preco: 12.99, img: "condicionador-tresemme.png" },
+    { nome: "Base de Maquiagem Natura", preco: 25.99, img: "base-de-maquiagem-natura.jpeg" },
+    { nome: "Rímel Maybelline", preco: 18.99, img: "rimel-maybelline.avif" },
+    { nome: "Batom L'Oréal", preco: 22.99, img: "batom-loreal.webp" },
+    { nome: "Creme de Pele Olay", preco: 30.99, img: "creme-de-pele-olay.webp" },
+    { nome: "Desodorante Dove", preco: 10.99, img: "desodorante-dove.avif" },
+    { nome: "Gel de Banho Nivea", preco: 14.99, img: "gel-de-banho-nivea.png" },
+    { nome: "Loção Corporal Vichy", preco: 28.99, img: "locao-corporal-vichy.avif" },
+    { nome: "Máscara de Cílios Lancôme", preco: 35.99, img: "mascara-de-cilios-lancome.webp" },
+    { nome: "Pó de Maquiagem MAC", preco: 32.99, img: "po-de-maquiagem-mac.avif" },
+    { nome: "Bronzer Guerlain", preco: 29.99, img: "bronzer-guerlain.avif" },
+    { nome: "Creme de Olhos La Roche-Posay", preco: 26.99, img: "creme-de-olhos-la-roche-posay.webp" },
+    { nome: "Shampoo Anticaspas Orico", preco: 19.99, img: "shampoo-anticaspas-orico.webp" },
+    { nome: "Condicionador de Cabelo", preco: 24.99, img: "condicionador-de-cabelo.webp" },
+    { nome: "Base de Unhas Sally Hansen", preco: 20.99, img: "base-de-unhas-sally-hansen.jpg" },
+    { nome: "Esmalte de Unhas Revlon", preco: 16.99, img: "esmalte-de-unhas-revlon.webp" },
+    { nome: "Creme de Mão Neutrogena", preco: 23.99, img: "creme-de-mao-neutrogema.webp" },
+    { nome: "Desodorante Antitranspirante Old Spice", preco: 27.99, img: "desodorante-antitranspirante-old-spice.jpg" },
+    { nome: "Gel de Banho Infantil Johnson's", preco: 11.99, img: "gel-de-banho-infantil-johnsons.jpg" },
+    { nome: "Loção Corporal para Bebês Pampers", preco: 17.99, img: "locao-corporal-para-bebes-pampers.jpeg" }
+  ];
+
+export default listaCosmeticos;
---
+++ scripts/show-items.js
@@ -0,0 +1,4 @@
+import listaMedicamentos from "../database/remedios.js";
+import listaCosmeticos from "../database/cosmeticos.js";
+
+console.log("ok")
